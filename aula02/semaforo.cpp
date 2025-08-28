c54eb132006273b9c6707a3d115e1f847d23d227
Leandro Cardoso <leandrocardosodev@gmail.com>
2025-08-28 00:50:48 -0300

    Aula 02 - Semaforo

---
+++ aula02/semaforo.cpp
@@ -0,0 +1,40 @@
+const int green_time = 10 * 1000;
+const int yellow_time = 3 * 1000;
+const int red_time = 10 * 1000;
+
+const int green_led = 7;
+const int yellow_led = 4;
+const int red_led = 2;
+
+void led_cicle(int pin, int time) {
+
+    digitalWrite(pin, HIGH);
+    delay(time);
+    digitalWrite(pin, LOW);
+
+}
+
+void setup() {
+
+    // put your setup code here, to run once:
+    pinMode(green_led, OUTPUT);
+    pinMode(yellow_led, OUTPUT);
+    pinMode(red_led, OUTPUT);
+
+    Serial.begin(115200);
+
+}
+
+void loop() {
+
+    // put your main code here, to run repeatedly:
+    Serial.println("Verde acendeu !!!");
+    led_cicle(green_led, green_time);
+
+    Serial.println("Amarelo acendeu !!!");
+    led_cicle(yellow_led, yellow_time);
+    
+    Serial.println("Vermelho acendeu !!!");
+    led_cicle(red_led, red_time);
+
+}
